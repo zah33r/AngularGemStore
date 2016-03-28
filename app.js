21312c2114c372618a59aa77a0f3026d52169825
zah33r <zaheer_sagitarius@hotmail.com>
2016-03-28 13:39:25 +0500

    Moving products to a new angular Module

--- app.js
+++ app.js
@@ -1,74 +1,20 @@
 (function() {
-    var app = angular.module('gemStore', []);
-
-    app.directive("productGallery", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-gallery.html",
-            controller: function() {
-                this.current = 0;
-                this.setCurrent = function(imageNumber) {
-                    this.current = imageNumber || 0;
-                };
-            },
-            controllerAs: "gallery"
-        };
-    });
+    var app = angular.module('gemStore', ['store-directives']);
 
     app.controller('StoreController', function() {
         this.products = gems;
     });
 
-    app.controller("ReviewController", function() {
-
+    app.controller('ReviewController', function() {
         this.review = {};
 
         this.addReview = function(product) {
             product.reviews.push(this.review);
+
             this.review = {};
         };
-
     });
 
-    app.directive("productDescriptions", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-description.html"
-        };
-    });
-
-    app.directive("productReviews", function() {
-        return {
-            restrict: 'E',
-            templateUrl: "product-reviews.html"
-        };
-    });
-
-    app.directive("productSpecs", function() {
-        return {
-            restrict: "A",
-            templateUrl: "product-specs.html"
-        };
-    });
-
-    app.directive("productTabs", function() {
-        return {
-            restrict: "E",
-            templateUrl: "product-tabs.html",
-            controller: function() {
-                this.tab = 1;
-
-                this.isSet = function(checkTab) {
-                    return this.tab === checkTab;
-                };
-
-                this.setTab = function(activeTab) {
-                    this.tab = activeTab;
-                };
-            },
-            controllerAs: "tab"
-        };
-    });
 
     var gems = [{
         name: 'Azurite',
@@ -83,15 +29,7 @@
             "images/gem-05.gif",
             "images/gem-09.gif"
         ],
-        reviews: [{
-            stars: 5,
-            body: "I love this gem!",
-            author: "joe@example.org"
-        }, {
-            stars: 1,
-            body: "This gem sucks.",
-            author: "tim@example.org"
-        }]
+        reviews: []
     }, {
         name: 'Bloodstone',
         description: "Origin of the Bloodstone is unknown, hence its low value. It has a very high shine and 12 sides, however.",
@@ -103,17 +41,9 @@
         images: [
             "images/gem-01.gif",
             "images/gem-03.gif",
-            "images/gem-04.gif"
+            "images/gem-04.gif",
         ],
-        reviews: [{
-            stars: 3,
-            body: "I think this gem was just OK, could honestly use more shine, IMO.",
-            author: "JimmyDean@example.org"
-        }, {
-            stars: 4,
-            body: "Any gem with 12 faces is for me!",
-            author: "gemsRock@example.org"
-        }]
+        reviews: []
     }, {
         name: 'Zircon',
         description: "Zircon is our most coveted and sought after gem. You will pay much to be the proud owner of this gorgeous and high shine gem.",
@@ -127,18 +57,6 @@
             "images/gem-07.gif",
             "images/gem-08.gif"
         ],
-        reviews: [{
-            stars: 1,
-            body: "This gem is WAY too expensive for its rarity value.",
-            author: "turtleguyy@example.org"
-        }, {
-            stars: 1,
-            body: "BBW: High Shine != High Quality.",
-            author: "LouisW407@example.org"
-        }, {
-            stars: 1,
-            body: "Don't waste your rubles!",
-            author: "nat@example.org"
-        }]
+        reviews: []
     }];
 })();
